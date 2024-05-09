6aa16126bb82f8bc043f75d0a5260cbc0235b265
Anthony <113693735+AnthonyCostaCrosciatte@users.noreply.github.com>
2024-05-09 10:10:24 -0300

    Update Atv03.js

---
+++ src/Atv03.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+
+
+export default function Atv03() {
+    return (
+        <>
+          <h1> Atividade 03</h1>
+         
+          <Link to="/">Retornar a página inicial </Link>
+        
+        </>
+    );
+}
