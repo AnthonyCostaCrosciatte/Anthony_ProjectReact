ce341e0c3326f5fdfcd2617f8327895f5d1e3dab
Anthony <113693735+AnthonyCostaCrosciatte@users.noreply.github.com>
2024-05-09 00:06:24 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Relogio from './Relogio';
-import Letreiro from './Letreiro';
+import MinhasRotas from './minhasRotas';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <Relogio />
-    <Letreiro/>
+    <MinhasRotas />
   </>
 );
 
