59ee675c4e46e92ac63b1f1c270a2ee5fa26339e
Anthony <113693735+AnthonyCostaCrosciatte@users.noreply.github.com>
2024-06-02 15:37:59 -0300

    Update funcaoToolbar.js

--- src/funcaoToolbar.js
+++ src/funcaoToolbar.js
@@ -4,35 +4,46 @@ import { sculptureList } from './DataAtv04';
 export default function Gallery() {
     const [index, setIndex] = useState(0);
     const [showMore, setShowMore] = useState(false);
+
     function handleNextClick() {
-        setIndex(index + 1);
+        if (index < sculptureList.length - 1) {
+            setIndex(index + 1);
+        }
     }
+
+    function handleBackClick() {
+        if (index > 0) {
+            setIndex(index - 1);
+        }
+    }
+
     function handleMoreClick() {
         setShowMore(!showMore);
     }
+
     let sculpture = sculptureList[index];
+
     return (
         <>
             <h3>Gallery</h3>
-            <button onClick={handleNextClick}>
+            <button onClick={handleBackClick} disabled={index === 0}>
+                Back
+            </button>
+            <button onClick={handleNextClick} disabled={index === sculptureList.length - 1}>
                 Next
             </button>
             <h2>
-                <i>{sculpture.name} </i>
-                by {sculpture.artist}
+                <i>{sculpture.name}</i> by {sculpture.artist}
             </h2>
             <h3>
                 ({index + 1} of {sculptureList.length})
             </h3>
             <button onClick={handleMoreClick}>
                 {showMore ? 'Hide' : 'Show'} details
-            </button >
-            <br/>
+            </button>
+            <br />
             {showMore && <p>{sculpture.description}</p>}
-            <img
-                src={sculpture.url}
-                alt={sculpture.alt}
-            />
+            <img src={sculpture.url} alt={sculpture.alt} />
         </>
     );
 }
