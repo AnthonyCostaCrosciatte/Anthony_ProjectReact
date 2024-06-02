fe54a41ea40e658565778a07c57e2221fdc285a9
Anthony <113693735+AnthonyCostaCrosciatte@users.noreply.github.com>
2024-06-02 15:04:54 -0300

    Add files via upload

--- src/minhasRotas.js
+++ src/minhasRotas.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import Atv01 from "./Atv01";
 import Atv02 from "./Atv02";
 import Atividade03 from "./Atv03"; 
+import Ativ04 from "./Atv04";
 
 export default function MinhasRotas() {
     return (
@@ -12,6 +13,7 @@ export default function MinhasRotas() {
                 <Route path="/atv01" element={<Atv01 />} />
                 <Route path="/atv02" element={<Atv02 />} />
                 <Route path="/atv03" element={<Atividade03 />} /> 
+                <Route path="/atv04" element={<Ativ04 />} /> 
             </Routes>
         </BrowserRouter>
     );
